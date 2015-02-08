0535897425b9275d0438a6ddf72d7557ff7ec0ec
Rafael Goncalves <goncalvjr@gmail.com>
2015-02-08 11:23:59 -0800

    batch

--- run-generator.bat
+++ run-generator.bat
@@ -2,15 +2,18 @@
 :: the form generator will be accessible (via a Web browser) at http://localhost:8085. Java 1.7+ and Ant are required.
 ::
 :: Compile sources and produce the war and javadocs
-echo Building from sources... && ant
+@echo off
+set port=8085
+echo.
+echo Building from sources...
+echo.
+call ant
 ::
 :: Deploy form-generator.war on a local Jetty instance, in the port specified below
 ::
-set port=8085
 echo.
-echo ! Starting Jetty server on port %port%...
+echo Starting Jetty server on port %port%...
 echo.
-echo ! Once the server is running, point your Web browser to http://localhost:$port to generate and submit forms
+echo Once the server is running, point your Web browser to http://localhost:%port% to generate and submit forms
 echo.
-:: open http://localhost:%port%
 java -jar jetty\jetty-runner.jar --port %port% form-generator.war
